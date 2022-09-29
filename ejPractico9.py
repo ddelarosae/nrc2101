df6119128a8c2855d6342230317e213f343e815f
ddelarosae <ddelarose@uninorte.edu.co>
2022-09-29 15:18:26 -0500

    correccion de errores

--- ejPractico9.py
+++ ejPractico9.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request, flash
 import utils
 import yagmail
+import os
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 @app.route('/')
 def index():
     return render_template('sesion.html')
 @app.route('/login')
 def login():
     return render_template('ejPractico9_login.html')
-@app.route('/register',methods=('GET','POST'))
-def register():
+@app.route('/sesion',methods=('GET','POST'))
+def sesion():
     try:
         if request.method == 'POST':
             username = request.form['usuario']
@@ -18,15 +20,15 @@ def register():
             if not utils.isUsernameValid(username):
                 error="El usuario debe ser alfanumerico"
                 flash(error)
-                return render_template('error.html')
+                return render_template('sesion.html')
             if not utils.isPasswordValid(password):
                 error="La contraseña debe tener al menos 8 caracteres"
                 flash(error)
-                return render_template('error.html')
+                return render_template('sesion.html')
             if not utils.isEmailValid(email):
                 error="El email debe ser alfanumerico"
                 flash(error)
-                return render_template('error.html')
+                return render_template('sesion.html')
             yag=yagmail.SMTP('danieldelarosa.ruta1@utp.edu.co','Qn$w+?3x.ACg')
             yag.send(to=email, subject='Cuenta creada', contents='Se ha creado su usuario')
             flash('Verifique su correo electronico')
